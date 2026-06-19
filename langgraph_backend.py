027a0ca7545f463539dea90169a66a25129e87ef
Patel Rahul <rahulpatel16092005@gmail.com>
2026-06-19 10:50:59 +0530

    add backend

---
+++ langgraph_backend.py
@@ -0,0 +1,45 @@
+from langgraph.graph import StateGraph,START,END
+from langchain_groq import ChatGroq
+from langchain_core.messages import BaseMessage,HumanMessage
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from typing import TypedDict,Literal,Annotated
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm=ChatGroq(model="llama-3.3-70b-versatile")
+
+#define state
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage],add_messages]
+
+#defien node
+def chat_node(state: ChatState) -> ChatState:
+
+    messages = state['messages']
+    response=llm.invoke(messages)
+    return {'messages':[response]}
+
+#define graph
+graph=StateGraph(ChatState)
+
+#add node
+graph.add_node('chat_node',chat_node)
+
+#add edges
+graph.add_edge(START,'chat_node')
+graph.add_edge('chat_node',END)
+
+#add checkpoint
+checkpoint=InMemorySaver()
+
+#compile graph
+chatbot=graph.compile(checkpointer=checkpoint)
+
+initial_state={'messages':[HumanMessage(content='who i am?')]}
+
+CONFIG={'configurable':{'thread_id':'1'}}
+
+print(chatbot.invoke(initial_state,config=CONFIG)['messages'][-1].content)
